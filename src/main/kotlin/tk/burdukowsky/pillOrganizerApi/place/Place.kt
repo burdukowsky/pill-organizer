3a33cbccb1caa418dfe72378ad49781bc4500745
burdukowsky <burdukowskystas@gmail.com>
2018-09-11 12:13:28 +0700

    add test places to 'import.sql'

--- src/main/kotlin/tk/burdukowsky/pillOrganizerApi/place/Place.kt
+++ src/main/kotlin/tk/burdukowsky/pillOrganizerApi/place/Place.kt
@@ -1,15 +1,13 @@
 package tk.burdukowsky.pillOrganizerApi.place
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-import javax.persistence.Table
+import javax.persistence.*
 
 @Entity
 @Table(name = "places")
 class Place(
         @Id
-        @GeneratedValue
+        @SequenceGenerator(name = "places_sequence", sequenceName = "places_sequence", allocationSize = 5)
+        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "places_sequence")
         val id: Long = 0,
         val name: String = "",
         val description: String = ""
