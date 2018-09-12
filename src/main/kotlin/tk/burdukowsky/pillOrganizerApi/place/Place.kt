479c522f1faee384d9da6e5ab1b0cce8b2fe21ab
burdukowsky <burdukowskystas@gmail.com>
2018-09-12 13:58:17 +0700

    add ManyToMany relation between Place and Pill

--- src/main/kotlin/tk/burdukowsky/pillOrganizerApi/place/Place.kt
+++ src/main/kotlin/tk/burdukowsky/pillOrganizerApi/place/Place.kt
@@ -1,5 +1,6 @@
 package tk.burdukowsky.pillOrganizerApi.place
 
+import tk.burdukowsky.pillOrganizerApi.pill.Pill
 import javax.persistence.*
 
 @Entity
@@ -11,5 +12,11 @@ class Place(
         val id: Long = 0,
         val name: String = "",
         @Column(columnDefinition = "text")
-        val description: String = ""
+        val description: String = "",
+        @ManyToMany(fetch = FetchType.EAGER, cascade = [CascadeType.MERGE])
+        @JoinTable(
+                name = "pills_places",
+                joinColumns = [JoinColumn(name = "place_id", referencedColumnName = "id")],
+                inverseJoinColumns = [JoinColumn(name = "pill_id", referencedColumnName = "id")])
+        var pills: List<Pill> = ArrayList()
 )
