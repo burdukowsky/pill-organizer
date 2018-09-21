db7dac08d9470f25fb9165046cc765bd1a544fdb
burdukowsky <burdukowskystas@gmail.com>
2018-09-21 17:34:42 +0700

    implement deletion pill places via REST

--- src/main/kotlin/tk/burdukowsky/pillOrganizerApi/place/PlaceController.kt
+++ src/main/kotlin/tk/burdukowsky/pillOrganizerApi/place/PlaceController.kt
@@ -1,9 +1,7 @@
 package tk.burdukowsky.pillOrganizerApi.place
 
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("places")
@@ -12,5 +10,12 @@ class PlaceController(private val placeRepository: PlaceRepository) {
     @GetMapping("")
     fun getPlaces(): ResponseEntity<List<Place>> {
         return ResponseEntity.ok().body(placeRepository.findAll())
+    }
+
+    @DeleteMapping("/{placeId}/pills/{pillId}")
+    fun deletePillPlace(@PathVariable(value = "placeId") placeId: Long,
+                        @PathVariable(value = "pillId") pillId: Long): ResponseEntity<Void> {
+        this.placeRepository.deletePillPlace(pillId, placeId)
+        return ResponseEntity.ok().build()
     }
 }
--- src/main/kotlin/tk/burdukowsky/pillOrganizerApi/place/PlaceRepository.kt
+++ src/main/kotlin/tk/burdukowsky/pillOrganizerApi/place/PlaceRepository.kt
@@ -1,5 +1,13 @@
 package tk.burdukowsky.pillOrganizerApi.place
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.transaction.annotation.Transactional
 
-interface PlaceRepository : JpaRepository<Place, Long>
+interface PlaceRepository : JpaRepository<Place, Long> {
+    @Transactional
+    @Modifying
+    @Query("delete from pills_places where pill_id = ?1 and place_id = ?2", nativeQuery = true)
+    fun deletePillPlace(pillId: Long, placeId: Long)
+}
