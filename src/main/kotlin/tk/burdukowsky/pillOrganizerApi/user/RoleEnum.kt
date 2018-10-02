90eba62334a16accef169861f2b4286bbebf304b
burdukowsky <burdukowskystas@gmail.com>
2018-10-02 12:14:20 +0700

    add EDITOR role

--- src/main/kotlin/tk/burdukowsky/pillOrganizerApi/user/RoleEnum.kt
+++ src/main/kotlin/tk/burdukowsky/pillOrganizerApi/user/RoleEnum.kt
@@ -1,5 +1,5 @@
 package tk.burdukowsky.pillOrganizerApi.user
 
 enum class RoleEnum {
-    ADMIN, VIEWER
+    ADMIN, EDITOR, VIEWER
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO roles (name) VALUES ('ADMIN'), ('VIEWER');
+INSERT INTO roles (name) VALUES ('ADMIN'), ('EDITOR'), ('VIEWER');
 INSERT INTO users (id, username, password) VALUES (1, 'username', '$2a$10$tYp8Q3ujZXSpM.d6xy6jaueLy/XSFVLaj6ms16xU2wnQ24UfT1dBe');
-INSERT INTO users_roles (user_id, role_name) VALUES (1, 'ADMIN'), (1, 'VIEWER');
+INSERT INTO users_roles (user_id, role_name) VALUES (1, 'ADMIN'), (1, 'EDITOR'), (1, 'VIEWER');
 INSERT INTO places (id, name, description) VALUES (1, 'Большая аптечка на шкафу', ''), (2, 'Маленькая аптечка на шкафу', '');
 INSERT INTO pills (id, name, description) VALUES (1, 'Диазолин', ''), (2, 'Парацетамол', '');
 INSERT INTO pills_places (pill_id, place_id) VALUES (1, 1), (1, 2), (2, 1);
